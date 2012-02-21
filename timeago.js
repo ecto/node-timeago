ddd47d53e22872ea9dd2492f4bc5ccddbff4fd50
Cam Pedersen <diffference@gmail.com>
2012-02-20 19:18:47 -0500

    [major] accept millisecond and epoch time

--- timeago.js
+++ timeago.js
@@ -8,18 +8,27 @@
  * http://ejohn.org/blog/javascript-pretty-date/
  */
 
-module.exports = function(time){
-  var date = new Date(time),
-      diff = (((new Date()).getTime() - date.getTime()) / 1000),
-      day_diff = Math.floor(diff / 86400);
-  if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 ) return;
+umodule.exports = function (time) {
+  var cur = +new Date() / 1000;
+  var date = +new Date(time);
+  var ms = date.toString().length > 10;
+  var day = 86400;
+
+  if (ms) date /= 1000;
+
+  var diff = Math.floor(cur - date);
+  var day_diff = Math.floor(diff / day);
+
+  console.log([cur, date, diff, day_diff]);
+
+  if (isNaN(day_diff) || day_diff < 0) return;
   return day_diff == 0 && (
-         diff < 60 && "just now" ||
-         diff < 120 && "1 minute ago" ||
-         diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-         diff < 7200 && "1 hour ago" ||
-         diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-         day_diff == 1 && "Yesterday" ||
-         day_diff < 7 && day_diff + " days ago" ||
-         day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+    diff < 60     &&                               "just now"  ||
+    diff < 120    &&                           "1 minute ago"  ||
+    diff < 3600   && Math.floor( diff / 60 ) + " minutes ago"  ||
+    diff < 7200   &&                             "1 hour ago"  ||
+    diff < 86400  && Math.floor( diff / 3600 ) + " hours ago") ||
+    day_diff == 1 &&                              "Yesterday"  ||
+    day_diff < 7  &&                   day_diff + " days ago"  ||
+                     Math.ceil( day_diff / 7 ) + " weeks ago";
 }
